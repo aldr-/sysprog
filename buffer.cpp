15986286d9db8b953007af874523b7037735535b
aldr- <aldr@aldr.de>
2015-02-21 17:57:02 +0100

    Buffer

---
+++ buffer.cpp
@@ -0,0 +1,18 @@
+/*
+ * buffer.cpp
+ *
+ *  Created on: Feb 21, 2015
+ *      Author: aldr
+ */
+
+#include "buffer.h"
+
+buffer::buffer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+buffer::~buffer() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ buffer.h
@@ -0,0 +1,17 @@
+/*
+ * buffer.h
+ *
+ *  Created on: Feb 21, 2015
+ *      Author: aldr
+ */
+
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+class buffer {
+public:
+	buffer();
+	virtual ~buffer();
+};
+
+#endif /* BUFFER_H_ */
