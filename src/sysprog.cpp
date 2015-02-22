0f54bf9aa217e95f99d741e119abc514ae0dc1b5
aldr- <aldr@aldr.de>
2015-02-22 17:54:42 +0100

    test

---
+++ src/sysprog.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : sysprog.cpp
+// Author      : aldr-
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
